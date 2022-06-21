cd7f5c957a594870adab43cbcf31598bbceba06a
Charlie McClung <charlie@bowtie.co>
2022-06-21 00:31:38 -0600

    stuff

--- src/tinker.ts
+++ src/tinker.ts
@@ -1,6 +1,9 @@
 import { F11Controller } from './core/Controller';
 
-const ctl = new F11Controller('0.0.0.0', [4444, 6969, 9999]);
+// const std = [4444, 6969, 9999];
+const xta = [3333, 4444, 4321, 5555, 6666, 6789, 6969, 7777, 9876, 9999]
+
+const ctl = new F11Controller('0.0.0.0', xta);
 
 ctl.gen(false, (err) => {
   if (err) {
