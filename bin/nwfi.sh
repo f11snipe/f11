7f6898cf8d2935da7bff20ad905d108906177897
Charlie McClung <charlie@bowtie.co>
2025-06-23 15:33:38 -0600

    test

--- bin/nwfi.sh
+++ bin/nwfi.sh
@@ -9,5 +9,5 @@ valid_port="^[0-9]+$"
 if [[ $rhost =~ $valid_ip ]] && [[ $rport =~ $valid_port ]]; then
   RHOST=$rhost RPORT=$rport node /home/charlie/dev/f11snipe/f11/src/wfi.js
 else
-  echo "Usage: $0 <remote_ip> <remote_port>"
+  echo "Usage: $(basename $0) <remote_ip> <remote_port>"
 fi
