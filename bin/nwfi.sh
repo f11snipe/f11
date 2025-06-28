f9174e0155cb22be3379e8dca8c7359bc43307cd
F11snipe <f11snipe@gmail.com>
2025-06-28 03:41:52 +0000

    fix relative nwfi

--- bin/nwfi.sh
+++ bin/nwfi.sh
@@ -7,7 +7,7 @@ valid_ip="^([0-9]{1,3}\.){3}[0-9]{1,3}$"
 valid_port="^[0-9]+$"
 
 if [[ $rhost =~ $valid_ip ]] && [[ $rport =~ $valid_port ]]; then
-  RHOST=$rhost RPORT=$rport node /home/charlie/dev/f11snipe/f11/src/wfi.js
+  RHOST=$rhost RPORT=$rport node ~/dev/f11snipe/f11/src/wfi.js
 else
   echo "Usage: $(basename $0) <remote_ip> <remote_port>"
 fi
