1672a28e8d923cbc3a44bb7604a4d8b060185e10
Charlie McClung <charlie@bowtie.co>
2025-07-02 10:09:04 -0600

    rebuild wfi script

--- src/wfi.js
+++ src/wfi.js
@@ -3,8 +3,8 @@ exports.__esModule = true;
 var net = require("net");
 var _a = process.env, _b = _a.RHOST, RHOST = _b === void 0 ? '127.0.0.1' : _b, _c = _a.RPORT, RPORT = _c === void 0 ? '80' : _c;
 var MAX_WAIT = 600000; // 10 minutes
-var INTERVAL = 100; // 100ms
-var TIMEOUT = 2000;
+var INTERVAL = 10; // 10ms
+var TIMEOUT = 500;
 var report = {};
 var categorize = function (data) {
     var cats = ['FTP', 'SSH', 'TELNET', 'MYSQL', 'SMB', 'SAMBA', 'RPC', 'BIND'];
--- src/wfi.ts
+++ src/wfi.ts
@@ -2,8 +2,8 @@ import * as net from 'net';
 
 const { RHOST = '127.0.0.1', RPORT = '80' } = process.env;
 const MAX_WAIT = 600000; // 10 minutes
-const INTERVAL = 100; // 100ms
-const TIMEOUT = 2000;
+const INTERVAL = 10; // 10ms
+const TIMEOUT = 500;
 const report: { [port: number]: string } = {};
 
 const categorize = (data: string): string[] => {
